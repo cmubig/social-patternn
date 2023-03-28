221e9ab6c9f0f4de7f9292c48e36cf0b01e6d4e6
Ingrid Navarro <24197463+navarrs@users.noreply.github.com>
2023-03-28 16:35:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(['sprnn', 'sprnn.*'], exclude=['tests']),
     install_requires=[
         'natsort==7.1.1',
-        'pandas==1.3.3',
+        'pandas==1.4.1',
         'pillow',
         'rsa==4.7.2',
         'scikit-learn==1.0.1',
@@ -14,7 +14,7 @@ setup(
         'tensorboard==2.6.0',
         'tensorboard-data-server==0.6.1',
         'tensorboard-plugin-wit==1.8.0',
-        'torch==1.8.1',
+        'torch',
         'tqdm==4.61.2',
         'protobuf==3.19.4',
     ],
